86333703f96514a30902cb574c8a7da905e4650e
new_user_m.fouad <mohamed.fouad@3ddx.com>
2018-11-27 14:33:38 +0200

    speackers

--- server/routes/speakers/index.js
+++ server/routes/speakers/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 module.exports =()=>{
     router.get('/',(req, res)=>{
-        return res.send('Al Speakers');
+        return res.render('speakers');
     });
     router.get('/:name',(req, res)=>{
         return res.send(`speakers detal page ${req.params.name}`);
